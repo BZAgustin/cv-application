4e84c4502ce7fa9aa14752b6f8627b1601ddfecf
BZAgustin <abustoszanelli@gmail.com>
2023-08-09 10:49:23 -0300

    Place divs for Education and Work Exp preview

--- src/components/Preview.jsx
+++ src/components/Preview.jsx
@@ -4,12 +4,23 @@ function Preview({ contact, education, workExp }) {
   const { firstName, lastName, address, phone } = contact;
 
   return (
-    <div className="preview">
-      <h2>First Name: {firstName}</h2>
-      <h2>Last Name: {lastName}</h2>
-      <h2>Address: {address}</h2>
-      <h2>Phone: {phone}</h2>
-    </div>
+    <>
+      <div className="preview">
+        <h2>First Name: {firstName}</h2>
+        <h2>Last Name: {lastName}</h2>
+        <h2>Address: {address}</h2>
+        <h2>Phone: {phone}</h2>
+      </div>
+
+      <div className="education">
+
+      </div>
+
+      <div className="workExperience">
+        
+      </div>
+    </>
+    
   );
 }
 
