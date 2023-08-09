646e7d16ff757bf7c4e96000b5982a70f67b7145
BZAgustin <abustoszanelli@gmail.com>
2023-08-09 10:48:21 -0300

    Add Education component

---
+++ src/components/Education.jsx
@@ -0,0 +1,43 @@
+/* eslint-disable react/prop-types */
+
+function Education({ educationList }) {
+  return (
+    <div className="education">
+      <h1>Education Experiences</h1>
+
+      {educationList.map((education) => (
+        <form action="" key={education.id}>
+          <label htmlFor="institution">Institution</label>
+          <input 
+            type="text"
+            id={`institution-${education.id}`}
+            value={education.institution}
+          />
+
+          <label htmlFor="degree">Degree</label>
+          <input 
+            type="text"
+            id={`degree-${education.id}`}
+            value={education.degree} 
+          />
+
+          <label htmlFor="from">From</label>
+          <input 
+            type="date"
+            id={`from-${education.id}`} 
+            value={education.from}
+          />
+
+          <label htmlFor="to">To</label>
+          <input 
+            type="date"
+            id={`to-${education.id}`}
+            value={education.to}
+          />
+        </form>
+      ))}
+    </div>
+  );
+}
+
+export default Education;
