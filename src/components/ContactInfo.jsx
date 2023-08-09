ef8f2bc78ec745bfad0ed76367c8d57b52c2bad1
BZAgustin <abustoszanelli@gmail.com>
2023-08-08 22:45:13 -0300

    Add first components

--- src/components/ContactInfo.jsx
+++ src/components/ContactInfo.jsx
@@ -1,7 +1,46 @@
-function ContactInfo({ firstName, lastName, address, phone }) {
+/* eslint-disable react/prop-types */
+import '../styles/ContactInfo.css'
+
+function ContactInfo({ firstName, lastName, address, phone, changeFirstName,
+                       changeLastName, changeAddress, changePhone }) {
   return (
-    <>
-      
-    </>
+    <div className="editor">
+      <h1>Contact Info</h1>
+      <form action="">
+        <label htmlFor="firstName">First Name</label>
+        <input 
+          type="text"
+          id="firstName"
+          value={firstName}
+          onChange={changeFirstName} 
+        />
+
+        <label htmlFor="lastName">Last Name</label>
+        <input 
+          type="text"
+          id="lastName"
+          value={lastName}
+          onChange={changeLastName} 
+        />
+
+        <label htmlFor="address">Address</label>
+        <input 
+          type="text"
+          id="address"
+          value={address}
+          onChange={changeAddress} 
+        />
+
+        <label htmlFor="phone">Phone Number</label>
+        <input 
+          type="tel"
+          id="phone"
+          value={phone}
+          onChange={changePhone} 
+        />
+      </form>
+    </div>
   );
 }
+
+export default ContactInfo;
---
+++ src/components/Preview.jsx
@@ -0,0 +1,16 @@
+import '../styles/Preview.css'
+
+function Preview({ contact, education, workExp }) {
+  const { firstName, lastName, address, phone } = contact;
+
+  return (
+    <div className="preview">
+      <h2>First Name: {firstName}</h2>
+      <h2>Last Name: {lastName}</h2>
+      <h2>Address: {address}</h2>
+      <h2>Phone: {phone}</h2>
+    </div>
+  );
+}
+
+export default Preview;
