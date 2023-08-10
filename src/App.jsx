022bfab718414859ccb9b225014f6eb09cdf4d93
BZAgustin <abustoszanelli@gmail.com>
2023-08-09 23:09:35 -0300

    Add functions for handling state changes and button functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import ContactInfo from './components/ContactInfo';
 import Education from './components/Education';
+import Work from './components/Work';
 import Preview from './components/Preview';
 import uniqid from 'uniqid';
 import './App.css'
@@ -45,6 +46,17 @@ function App() {
     setContactInfo({ ...contactInfo, phone: e.target.value });
   }
 
+  const handleEducationChange = (id, field, value) => {
+    setEducation(prevEducation => {
+      return prevEducation.map(edu => {
+        if(edu.id === id) {
+          return { ...edu, [field]: value };
+        }
+        return edu;
+      });
+    });
+  }
+
   const addEducation = (e) => {
     e.preventDefault();
     setEducation([...education, {
@@ -54,6 +66,38 @@ function App() {
       from: '',
       to: ''
     }]);
+  }
+
+  const removeEducation = (e, id) => {
+    e.preventDefault();
+    setEducation(education.filter(item => item.id !== id));
+  }
+  
+  const handleWorkChange = (id, field, value) => {
+    setWorkExperience(prevWorkExp => {
+      return prevWorkExp.map(work => {
+        if(work.id === id) {
+          return { ...work, [field]: value };
+        }
+        return work;
+      });
+    });
+  }
+
+  const addWorkExperience = (e) => {
+    e.preventDefault();
+    setWorkExperience([...workExperience, {
+      id: uniqid(),
+      company: '',
+      position: '',
+      from: '',
+      to: ''
+    }]);
+  }
+
+  const removeWorkExperience = (e, id) => {
+    e.preventDefault();
+    setWorkExperience(workExperience.filter(work => work.id !== id));
   }
 
   return (
@@ -74,8 +118,17 @@ function App() {
 
             <Education 
               educationList={education}
+              changeEducation={handleEducationChange}
               newEducation={addEducation}
-           />
+              removeEducation={removeEducation}
+            />
+
+            <Work 
+              workExpList={workExperience}
+              changeWork={handleWorkChange}
+              newExp={addWorkExperience}
+              removeWorkExp={removeWorkExperience}
+            />
           </div>
 
           <div className="preview">
